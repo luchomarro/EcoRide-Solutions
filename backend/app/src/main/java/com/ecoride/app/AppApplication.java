98368dc50cdc4973ed1aa2a27b8248eb3e2b6e3d
luchomarro <luchomarro@gmail.com>
2026-06-22 02:48:18 -0500

    Correcciones Aplicadas ClaudeReview

--- backend/app/src/main/java/com/ecoride/app/AppApplication.java
+++ backend/app/src/main/java/com/ecoride/app/AppApplication.java
@@ -3,11 +3,9 @@ package com.ecoride.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ecoride.app") // <-- ESTO OBLIGA A SPRING A BUSCAR TUS CAPAS
 public class AppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }
